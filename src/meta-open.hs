8f74c75b20f16fe8efcd45eab0af329398f5cdce
Cary Robbins <carymrobbins@gmail.com>
2014-01-29 22:04:30 -0600

    Finished rough draft.

--- src/meta-open.hs
+++ src/meta-open.hs
@@ -8,6 +8,9 @@ type GrepKey = String
 type Command = String
 type GrepMap = [(GrepKey, Command)]
 
+defaultCommand :: Command
+defaultCommand = "open"
+
 data Program = IntelliJ
 
 programMap :: [(Program, GrepMap)]
@@ -19,12 +22,35 @@ fileTypeAssociations = undefined
 getGrepMapForFile :: FilePath -> GrepMap
 getGrepMapForFile = undefined
 
+findRunningFromGrepMap :: GrepMap -> IO [((GrepKey, Command), Bool)]
+findRunningFromGrepMap = undefined
+
+findFirstCommand :: [((GrepKey, Command), Bool)] -> Maybe Command
+findFirstCommand = undefined
+
+chooseCommand :: Maybe Command -> Command
+chooseCommand = undefined
+
 handleFile :: Maybe FilePath -> IO ()
-handleFile = undefined
+handleFile Nothing = putStrLn "Usage: meta-open [filename]"
+handleFile (Just filename) = do
+    let grepMap = getGrepMapForFile filename
+    running <- findRunningFromGrepMap grepMap
+    let command = chooseCommand . findFirstCommand $ running
+    runBash . unwords $ [command, filename]
 
 main :: IO ()
 main = liftM listToMaybe getArgs >>= handleFile
 
+pairM :: Monad m => (a -> m b) -> a -> m (a, b)
+pairM = undefined
+
+isRunning :: GrepKey -> IO Bool
+isRunning grepKey = undefined
+
 getBash :: String -> IO String
 getBash cmd = readProcess "bash" ["-c", cmd] ""
+
+runBash :: String -> IO ()
+runBash cmd = getBash cmd >> return ()
 
